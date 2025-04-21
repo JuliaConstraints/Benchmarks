2f11a811cf3509de23dae3e4b85ac0c2c3ce5879
Azzaare <jf@baffier.fr>
2025-04-21 15:09:30 +0900

    Exploration bug

--- ConstraintDomains/checks.jl
+++ ConstraintDomains/checks.jl
@@ -8,7 +8,8 @@ using PerfChecker
 d_commons = Dict(
     :targets => ["ConstraintDomains"],
     :path => @__DIR__,
-    :pkgs => ("ConstraintDomains", :custom, [v"0.2.5", v"0.3.0", v"0.3.1", v"0.3.2", v"0.3.3", v"0.3.4", v"0.3.5", v"0.3.6", v"0.3.7", v"0.3.8", v"0.3.9", v"0.3.10", v"0.3.11", v"0.3.12", v"0.3.13"], true),
+    :pkgs => ("ConstraintDomains", :custom, [v"0.2.5", v"0.3.0", v"0.3.1", v"0.3.2", v"0.3.3", v"0.3.4", v"0.3.5", v"0.3.6", v"0.3.7", v"0.3.8", v"0.3.9", v"0.3.10", v"0.3.11", v"0.3.13", v"0.3.14", v"0.3.15"], true),
+    :devops => "ConstraintDomains",
     :seconds => 100,
     :samples => 10,
     :evals => 10,
@@ -49,182 +50,182 @@ function visu(x, d, ::Val{:chairmark})
     end
 end
 
-## SECTION - Commons: benchmarks and chairmarks
+# ## SECTION - Commons: benchmarks and chairmarks
 
-@info "Running checks: Commons"
+# @info "Running checks: Commons"
 
-d = deepcopy(d_commons)
-d[:tags] = [:common]
+# d = deepcopy(d_commons)
+# d[:tags] = [:common]
 
-x = @check :benchmark d begin
-    using ConstraintDomains
-end begin
-    ed = domain()
-    domain_size(ed) == 0 == length(ed)
-    isempty(ed)
-    π ∉ ed
-end
+# x = @check :benchmark d begin
+#     using ConstraintDomains
+# end begin
+#     ed = domain()
+#     domain_size(ed) == 0 == length(ed)
+#     isempty(ed)
+#     π ∉ ed
+# end
 
-visu(x, d, Val(:benchmark))
+# visu(x, d, Val(:benchmark))
 
-x = @check :chairmark d begin
-    using ConstraintDomains
-end begin
-    ed = domain()
-    domain_size(ed) == 0 == length(ed)
-    isempty(ed)
-    π ∉ ed
-end
+# x = @check :chairmark d begin
+#     using ConstraintDomains
+# end begin
+#     ed = domain()
+#     domain_size(ed) == 0 == length(ed)
+#     isempty(ed)
+#     π ∉ ed
+# end
 
-visu(x, d, Val(:chairmark))
+# visu(x, d, Val(:chairmark))
 
-## SECTION - Continuous: benchmarks and chairmarks
+# ## SECTION - Continuous: benchmarks and chairmarks
 
-@info "Running checks: Continuous"
+# @info "Running checks: Continuous"
 
-d = deepcopy(d_commons)
-d[:tags] = [:continuous]
+# d = deepcopy(d_commons)
+# d[:tags] = [:continuous]
 
-x = @check :benchmark d begin
-    using ConstraintDomains
-    using Intervals
-end begin
-    if d[:current_version] < v"0.3.0"
-        d1 = domain((1.0, true), (3.15, true))
-        d2 = domain((-42.42, false), (5.0, false))
-    else
-        d1 = domain(1.0 .. 3.15)
-        d2 = domain(Interval{Open,Open}(-42.42, 5.0))
-    end
-    domains = [d1, d2]
-    for d in domains
-        for x in [1, 2.3, π]
-            x ∈ d
-        end
-        for x in [5.1, π^π, Inf]
-            x ∉ d
-        end
-        rand(d) ∈ d
-        rand(d, 1) ∈ d
-        domain_size(d) > 0.0
-    end
-end
+# x = @check :benchmark d begin
+#     using ConstraintDomains
+#     using Intervals
+# end begin
+#     if d[:current_version] == "dev" || d[:current_version] < v"0.3.0"
+#         d1 = domain((1.0, true), (3.15, true))
+#         d2 = domain((-42.42, false), (5.0, false))
+#     else
+#         d1 = domain(1.0 .. 3.15)
+#         d2 = domain(Interval{Open,Open}(-42.42, 5.0))
+#     end
+#     domains = [d1, d2]
+#     for d in domains
+#         for x in [1, 2.3, π]
+#             x ∈ d
+#         end
+#         for x in [5.1, π^π, Inf]
+#             x ∉ d
+#         end
+#         rand(d) ∈ d
+#         # rand(d, 1) ∈ d
+#         domain_size(d) > 0.0
+#     end
+# end
 
-visu(x, d, Val(:benchmark))
+# visu(x, d, Val(:benchmark))
 
-x = @check :chairmark d begin
-    using ConstraintDomains
-    using Intervals
-end begin
-    if d[:current_version] < v"0.3.0"
-        d1 = domain((1.0, true), (3.15, true))
-        d2 = domain((-42.42, false), (5.0, false))
-    else
-        d1 = domain(1.0 .. 3.15)
-        d2 = domain(Interval{Open,Open}(-42.42, 5.0))
-    end
-    domains = [d1, d2]
-    for d in domains
-        for x in [1, 2.3, π]
-            x ∈ d
-        end
-        for x in [5.1, π^π, Inf]
-            x ∉ d
-        end
-        rand(d) ∈ d
-        rand(d, 1) ∈ d
-        domain_size(d) > 0.0
-    end
-end
+# x = @check :chairmark d begin
+#     using ConstraintDomains
+#     using Intervals
+# end begin
+#     if d[:current_version] == "dev" || d[:current_version] < v"0.3.0"
+#         d1 = domain((1.0, true), (3.15, true))
+#         d2 = domain((-42.42, false), (5.0, false))
+#     else
+#         d1 = domain(1.0 .. 3.15)
+#         d2 = domain(Interval{Open,Open}(-42.42, 5.0))
+#     end
+#     domains = [d1, d2]
+#     for d in domains
+#         for x in [1, 2.3, π]
+#             x ∈ d
+#         end
+#         for x in [5.1, π^π, Inf]
+#             x ∉ d
+#         end
+#         rand(d) ∈ d
+#         # rand(d, 1) ∈ d
+#         domain_size(d) > 0.0
+#     end
+# end
 
-visu(x, d, Val(:chairmark))
+# visu(x, d, Val(:chairmark))
 
-## SECTION - Discrete: benchmarks and chairmarks
+# ## SECTION - Discrete: benchmarks and chairmarks
 
-@info "Running checks: Discrete"
+# @info "Running checks: Discrete"
 
-d = deepcopy(d_commons)
-d[:tags] = [:discrete]
+# d = deepcopy(d_commons)
+# d[:tags] = [:discrete]
 
-x = @check :benchmark d begin
-    using ConstraintDomains
-end begin
-    d1 = domain([4, 3, 2, 1])
-    d2 = domain(1)
-    foreach(i -> add!(d2, i), 2:4)
-    domains = [d1, d2]
-    for d in domains
-        for x in [1, 2, 3, 4]
-            x ∈ d
-        end
-        length(d) == 4
-        rand(d) ∈ d
-        add!(d, 5)
-        5 ∈ d
-        delete!(d, 5)
-        5 ∉ d
-        domain_size(d) == 3
-    end
+# x = @check :benchmark d begin
+#     using ConstraintDomains
+# end begin
+#     d1 = domain([4, 3, 2, 1])
+#     d2 = domain(1)
+#     foreach(i -> add!(d2, i), 2:4)
+#     domains = [d1, d2]
+#     for d in domains
+#         for x in [1, 2, 3, 4]
+#             x ∈ d
+#         end
+#         length(d) == 4
+#         rand(d) ∈ d
+#         add!(d, 5)
+#         5 ∈ d
+#         delete!(d, 5)
+#         5 ∉ d
+#         domain_size(d) == 3
+#     end
 
-    d3 = domain(1:5)
-    d4 = domain(1:0.5:5)
-    domains2 = [d3, d4]
-    for d in domains2
-        for x in [1, 2, 3, 4, 5]
-            x ∈ d
-        end
-        for x in [42]
-            x ∉ d
-        end
-        rand(d) ∈ d
-    end
-end
+#     d3 = domain(1:5)
+#     d4 = domain(1:0.5:5)
+#     domains2 = [d3, d4]
+#     for d in domains2
+#         for x in [1, 2, 3, 4, 5]
+#             x ∈ d
+#         end
+#         for x in [42]
+#             x ∉ d
+#         end
+#         rand(d) ∈ d
+#     end
+# end
 
-visu(x, d, Val(:benchmark))
+# visu(x, d, Val(:benchmark))
 
-x = @check :chairmark d begin
-    using ConstraintDomains
-end begin
-    d1 = domain([4, 3, 2, 1])
-    d2 = domain(1)
-    foreach(i -> add!(d2, i), 2:4)
-    domains = [d1, d2]
-    for d in domains
-        for x in [1, 2, 3, 4]
-            x ∈ d
-        end
-        length(d) == 4
-        rand(d) ∈ d
-        add!(d, 5)
-        5 ∈ d
-        delete!(d, 5)
-        5 ∉ d
-        domain_size(d) == 3
-    end
+# x = @check :chairmark d begin
+#     using ConstraintDomains
+# end begin
+#     d1 = domain([4, 3, 2, 1])
+#     d2 = domain(1)
+#     foreach(i -> add!(d2, i), 2:4)
+#     domains = [d1, d2]
+#     for d in domains
+#         for x in [1, 2, 3, 4]
+#             x ∈ d
+#         end
+#         length(d) == 4
+#         rand(d) ∈ d
+#         add!(d, 5)
+#         5 ∈ d
+#         delete!(d, 5)
+#         5 ∉ d
+#         domain_size(d) == 3
+#     end
 
-    d3 = domain(1:5)
-    d4 = domain(1:0.5:5)
-    domains2 = [d3, d4]
-    for d in domains2
-        for x in [1, 2, 3, 4, 5]
-            x ∈ d
-        end
-        for x in [42]
-            x ∉ d
-        end
-        rand(d) ∈ d
-    end
-end
+#     d3 = domain(1:5)
+#     d4 = domain(1:0.5:5)
+#     domains2 = [d3, d4]
+#     for d in domains2
+#         for x in [1, 2, 3, 4, 5]
+#             x ∈ d
+#         end
+#         for x in [42]
+#             x ∉ d
+#         end
+#         rand(d) ∈ d
+#     end
+# end
 
-visu(x, d, Val(:chairmark))
+# visu(x, d, Val(:chairmark))
 
-## SECTION - Explore: benchmarks and chairmarks
+# ## SECTION - Explore: benchmarks and chairmarks
 
-@info "Running checks: Explore"
+# @info "Running checks: Explore"
 
 d = deepcopy(d_commons)
 d[:tags] = [:explore]
-d[:pkgs] = ("ConstraintDomains", :custom, [v"0.3.1", v"0.3.2", v"0.3.3", v"0.3.4", v"0.3.5", v"0.3.6", v"0.3.7", v"0.3.8", v"0.3.9", v"0.3.10", v"0.3.11", v"0.3.12", v"0.3.13"], true)
+d[:pkgs] = ("ConstraintDomains", :custom, [v"0.3.1", v"0.3.2", v"0.3.3", v"0.3.4", v"0.3.5", v"0.3.6", v"0.3.7", v"0.3.8", v"0.3.9", v"0.3.10", v"0.3.11", v"0.3.13", v"0.3.14", v"0.3.15"], true)
 
 x = @check :benchmark d begin
     using ConstraintDomains
@@ -247,6 +248,17 @@ end begin
 end
 
 visu(x, d, Val(:chairmark))
+
+# x = @check :alloc d begin
+#     using ConstraintDomains
+# end begin
+#     domains = [domain([1, 2, 3, 4]) for i = 1:4]
+#     X, X̅ = explore(domains, allunique)
+#     length(X) == factorial(4)
+#     length(X̅) == 4^4 - factorial(4)
+# end
+
+# visu(x, d, Val(:allocs))
 
 # TODO: add more checks for parameters.jl
 
